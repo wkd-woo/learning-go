68a28392beea962cfc52395e18da40c133449e39
wkd-woo <wkdwoos@gmail.com>
2022-11-20 22:59:27 +0900

    ch2

---
+++ ch2/type_promotion.go
@@ -0,0 +1,10 @@
+package main
+
+func main() {
+	var x int = 10
+	var y float64 = 30.2
+	var z float64 = float64(x) + y
+	var d int = x + int(y)
+
+	print(z, d)
+}
---
+++ ch2/var_versus.go
@@ -0,0 +1,15 @@
+package main
+
+func main() {
+	var (
+		one   int
+		two       = 20
+		three int = 30
+		d, e      = 40, "hello"
+		f, g  string
+	)
+
+	x, y := 30, "hello"
+
+	print(x, y, one, two, three, d, e, f, g)
+}
